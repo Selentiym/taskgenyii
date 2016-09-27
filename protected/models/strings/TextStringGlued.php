1d16bfa8d1ed74f0ddfa8d4806c58ffbe0cc7ed2
Bondartsev Nikita <bondartsev.nikita@gmail.com>
2016-09-27 18:38:20 +0300

    Fixed another case mismatch

--- protected/models/strings/TextStringGlued.php
+++ protected/models/strings/TextStringGlued.php
@@ -6,7 +6,7 @@
  * Date: 23.09.2016
  * Time: 21:15
  */
-class TextStringGlued extends TextString{
+class TextStringGlued extends textString{
     public function addNewSentence($text){
         if ($text) {
             $this -> sentences [] = new wordSetGlued($text);
